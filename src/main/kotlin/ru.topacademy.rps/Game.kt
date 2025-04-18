620d69f4110b42213e85727541d1337d8511991d
Qwexaro <Sergikstar25.gold@mail.ru>
2025-04-18 09:55:44 +0300

    Main

--- src/main/kotlin/ru.topacademy.rps/Game.kt
+++ src/main/kotlin/ru.topacademy.rps/Game.kt
@@ -3,10 +3,24 @@ package ru.topacademy.rps
 object Game {
 
     fun launch() {
-        val code = readln().toInt()
+        println("Figures: ")
 
+        Figure.entries.forEach {
+            val code = it.code
+            val name = it.name
+
+            val row = "[$code] $name"
+            println(row)
+        }
+
+        println()
+
+        print("Your figure code: ")
+        val code = readln().toInt()
         val figure1 = Figure.of(code) ?: error("Unknown code!")
+
         val figure2 = Figure.rand()
+        println("PC: ${figure2.code} (${figure2.name})")
 
         val res = result(figure1 to figure2)
 
@@ -14,14 +28,14 @@ object Game {
             println("DRAW!")
         } else {
             if (res) {
-                println("WIN: Human")
-            } else {
                 println("WIN: PC")
+            } else {
+                println("WIN: Human")
             }
         }
     }
 
-    fun result(figures: Pair<Figure, Figure>) = if (figures.first.code == figures.first.code) {
+    private fun result(figures: Pair<Figure, Figure>) = if (figures.first.code == figures.second.code) {
         null
     } else {
         if (figures.first.loss == figures.second.code) {
